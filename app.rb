856620542321e4c17e210525981ca2d496400706
Riley Slayden <slaydenriley@gmail.com>
2020-02-12 17:29:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Riley"
+  end
+
+  get '/hometown' do
+    "My hometown is Bonner"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Something in the Way She Moves'"
+  end
 end
